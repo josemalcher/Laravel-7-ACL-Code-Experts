cc23e87e47d5379aa843b5c5ef46c64fe79bd947
Jose Malcher Jr <contato@josemalcher.net>
2022-11-03 11:37:12 -0300

    08 - Mapeando Models

--- forum/app/Reply.php
+++ forum/app/Reply.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+
+    protected $fillable = ['reply'];
+
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
 }
--- forum/app/Thread.php
+++ forum/app/Thread.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+
+    protected $fillable = ['title', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
