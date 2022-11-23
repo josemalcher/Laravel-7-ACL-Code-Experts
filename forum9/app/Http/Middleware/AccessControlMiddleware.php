5fe15ef2b2cb4bd583d4948a22d4c25cce86a656
Jose Malcher Jr <contato@josemalcher.net>
2022-11-22 22:18:26 -0300

    45 - Ignorando Check de Permissão

--- forum9/app/Http/Middleware/AccessControlMiddleware.php
+++ forum9/app/Http/Middleware/AccessControlMiddleware.php
@@ -14,14 +14,18 @@ class AccessControlMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse) $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
+        $ignoreResources = config('accescontrollist')['ignore.resources'];
 
-        $this->authorize($request->route()->getName());
+        if (!in_array($request->route()->getName(), $ignoreResources)) {
+            $this->authorize($request->route()->getName());
+        }
+
 
         return $next($request);
     }
